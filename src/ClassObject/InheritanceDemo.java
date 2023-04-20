8b5cf4b656546fc4fa2e62f98f45e1629e450e9b
bodkhenamrata6 <bodkhenamrata6@gmail.com>
2023-04-20 09:52:45 +0530

    Abstract and Interface

--- src/ClassObject/InheritanceDemo.java
+++ src/ClassObject/InheritanceDemo.java
@@ -5,16 +5,21 @@ public class InheritanceDemo  {
 	public static void main(String[] args) {
 		InheritanceA parent = new InheritanceA();
 		InheritanceB child = new InheritanceB();
+		InheritanceC childofA =new InheritanceC(100,200);
 		
-		parent.a=10;
-		parent.b=20;
-		parent.showab();
-		
+		//parent.a=10;
+		//parent.b=20;
+		//parent.showab();
+	/*	
 		child.a=30;
 		child.b=40;
 		child.c=50;
 		child.showab();
 		child.showc();
+		*/
+		
+		
+		childofA.show();
 		
 
 	}
